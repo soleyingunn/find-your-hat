26ca1c25b7c90350560442b0b719c67caa5e7c1d
Lava <svanbergsdottir@gmail.com>
2023-09-14 16:42:03 -1000

    - create field class - constructor should be two dimentional array - print method that describes the current state

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(fieldArr) {
+        this._field = fieldArr;
+    }
+
+    print() {
+        for (let i = 0; i < this._field.length; i++) {
+            console.log(this._field[i].join(''));
+        }
+    }
+};
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+myField.print();
