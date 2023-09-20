5faa21f8a02a18ed6c2f74abaa2161df65cec66f
Lava <svanbergsdottir@gmail.com>
2023-09-20 11:02:59 -1000

    - Methods to test whether the current location results in win (user is on the hat) or a loss (user is on a hole or out-of-bounds). - A method to handle asking and accepting user input, and updating the current location. - A method to run the main game loop until the game is won or lost.

--- main.js
+++ main.js
@@ -14,6 +14,68 @@ class Field {
         this.field[0][0] = pathCharacter;
     }
 
+    runGame() {
+        let playing = true;
+        while (playing) {
+            this.print();
+            this.askQuestion();
+            if (!this.isInBounds()) {
+                console.log('Out of bounds instruction!');
+                playing = false;
+                break;
+            } else if (this.isHole()) {
+                console.log('Sorry, you fell down a hole!');
+                playing = false;
+                break;
+            } else if (this.isHat()) {
+                console.log('Congrats, you found your hat!');
+                playing = false;
+                break;
+            }
+            // Update the current location on the map
+            this.field[this.locationY][this.locationX] = pathCharacter;
+        }
+    }
+
+    askQuestion() {
+        const answer = prompt('Which way? ').toUpperCase();
+        switch (answer) {
+            case 'W':
+                this.locationY -= 1;
+                break;
+            case 'S':
+                this.locationY += 1;
+                break;
+            case 'A':
+                this.locationX -= 1;
+                break;
+            case 'D':
+                this.locationX += 1;
+                break;
+            default:
+                console.log('Enter W, A, S or D.');
+                this.askQuestion();
+                break;
+        }
+    }
+
+    isInBounds() {
+        return (
+            this.locationY >= 0 &&
+            this.locationX >= 0 &&
+            this.locationY < this.field.length &&
+            this.locationX < this.field[0].length
+        );
+    }
+
+    isHat() {
+        return this.field[this.locationY][this.locationX] === hat;
+    }
+
+    isHole() {
+        return this.field[this.locationY][this.locationX] === hole;
+    }
+
     print() {
         const displayString = this.field.map(row => {
             return row.join('');
